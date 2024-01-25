b4c6c0b4fa2b7ab3d1fea5df86bd6e91f4f23379
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-01-25 19:44:02 -0300

    Criando uma classe

---
+++ formacao_aprenda-a-programar-em-c#/orientacao-a-objetos/ScreenSound/ScreenSound/Music.cs
@@ -0,0 +1,20 @@
+﻿class Music {
+    public string name;
+    public string artist;
+    public int duratioin;
+    public bool available;
+
+    public void DisplayTechnicalSheet()
+    {
+        Console.WriteLine($"Nome: {name}");
+        Console.WriteLine($"Artista: {artist}");
+        Console.WriteLine($"Duração: {duratioin}");
+        if (available)
+        {
+            Console.WriteLine("Disponível!");
+        } else
+        {
+            Console.WriteLine("Adquire o plano plus!");
+        }
+    }
+}
---
+++ formacao_aprenda-a-programar-em-c#/orientacao-a-objetos/ScreenSound/ScreenSound/Program.cs
@@ -0,0 +1,16 @@
+﻿Music music1 = new Music();
+music1.name = "Impossible";
+music1.artist = "James Arthur";
+music1.duratioin = 209;
+music1.available = true;
+
+
+Music music2 = new Music();
+music2.name = "Hymn for the Weekend";
+music2.artist = "Coldplay";
+music2.duratioin = 258;
+music2.available = false;
+
+
+music1.DisplayTechnicalSheet();
+music2.DisplayTechnicalSheet();
