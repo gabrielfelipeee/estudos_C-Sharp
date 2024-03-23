f001d66dbe464177389bd8d446a32b13a8b01591
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-03-23 18:58:10 -0300

    Condições if else

---
+++ curso_c#/condicoes_if-else/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Condicoes
+{
+    static void Main()
+    {
+        float nota = 40f;
+        string result;
+
+
+        if (nota < 50)
+        {
+            result = "Reprovado";
+        } else if (nota < 60)
+        {
+            result = "Recuperação";
+        } else
+        {
+            result = "Aprovado";
+        }
+
+
+
+        // condição aninhada => Uma estrutura dentro da outra
+        var num = 99;
+
+        if (num > 0)
+        {
+            Console.WriteLine("É um número positivo");
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Também é um número par");
+            }
+            else
+            {
+                Console.WriteLine("Também é um número impar");
+            }
+        }
+        else
+        {
+            Console.WriteLine("É um número negativo");
+        }
+    }
+}
