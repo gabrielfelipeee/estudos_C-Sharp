06cb98a8cd7fd00b301023815cf0aadff51b8b84
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-03-23 08:01:39 -0300

    Enum e typecast

---
+++ curso_c#/enum_and_typecast/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Enum
+
+{
+    // Enum
+    enum DiasSemana {Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado};
+    static void Main()
+    {
+        DiasSemana domingo = DiasSemana.Domingo; // Domingo
+
+        // Por índice
+        DiasSemana segunda = (DiasSemana)1; // Segunda
+
+        
+
+
+        // Typecast => Converter um tipo em outro
+        float n1 = 10.5f;
+        int n2 = (int)n1; // type cast => convertendo o float em um int
+
+
+        short nShort = 10;
+        int nInt = (int)nShort;
+    }
+}
