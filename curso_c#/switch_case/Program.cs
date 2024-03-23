eb23b6e111a44d830edd9dd5428c12360be70639
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-03-23 19:30:58 -0300

    Switch case

---
+++ curso_c#/switch_case/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+
+class Switch {
+    static void Main()
+    {
+        int tempo;
+        char escolhaTransporte;
+        string transporte = "";
+
+        Console.WriteLine("Belo Horizonte para Vitória");
+        Console.WriteLine("Escolha o transporte: [A] Avião | [B] Ônibus | [C] Carro");
+        escolhaTransporte = char.Parse(Console.ReadLine());
+
+
+        switch (escolhaTransporte)
+        {
+            case 'A':
+                tempo = 60;
+                transporte = "Avião";
+                break;
+            case 'B':
+                tempo = 300;
+                transporte = "Ônibus";
+                break;
+            case 'C':
+                tempo = 180;
+                transporte = "Carro";
+                break;
+            default:
+                tempo = -1;
+                break;
+        }
+
+        if (tempo < 0)
+        {
+            Console.WriteLine("Transporte indisponível");
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Transporte escolhido: {0}", transporte);
+            Console.WriteLine("Tempo de viagem: {0} hora(s)", tempo / 60);
+        }
+    }
+}
