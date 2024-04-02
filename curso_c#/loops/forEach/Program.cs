a31732597910ce546dc948feb010fe8e17ce132c
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-04-02 11:33:23 -0300

    foreach

---
+++ curso_c#/loops/forEach/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class ForEach
+{
+    static void Main()
+    {
+        string[] bigSix = new string[6] { "Liverpool", "Man United", "Chelsea", "Arsenal", "Tottenham", "Man City" };
+
+        foreach (string item in bigSix)
+        {
+            Console.WriteLine(item); // Imprime cada elemento 
+        }
+
+    }
+}
