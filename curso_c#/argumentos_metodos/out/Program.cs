67b1f276b9763234398b07d26c411c556b282962
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-04-04 15:30:49 -0300

    Argumentos ref e out

---
+++ curso_c#/argumentos_metodos/out/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Out
+{
+
+    // out permite que o método retorne mais de um valor
+
+    static void Main()
+    {
+        int rest; // O resto será armazenado nessa variável
+        int quoc = Dividir(11, 2, out rest);
+
+        Console.WriteLine("Resultado da divisão: {0}",quoc);
+        Console.WriteLine("Resto da divisão: {0}",rest);
+    }
+
+    static int Dividir(int dividendo, int divisor, out int resto)
+    {
+        int quociente;
+        quociente = dividendo / divisor;
+        resto = dividendo % divisor;
+
+        return quociente;
+    }
+
+}
---
+++ curso_c#/argumentos_metodos/ref/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Ref
+{
+    static void Main()
+    {
+
+
+        // Passagem por referência
+        int num = 10;
+
+        // Com o ref: o argumento irá substituir o espaço na memória da variável num;
+        // Sem o ref: o argumento será criado em um novo espaço, e o num continuaria sendo 10;
+        Dobrar(ref num);
+
+        // Com ref irá imprimir: 20 | sem o ref: 10
+        Console.WriteLine(num);
+
+
+
+        // Passagem por valor (sem o ref)
+        int number = 100;
+        Desconto(number); // output: 90
+        Console.WriteLine(number);  // output: 100
+    }
+
+    static void Dobrar(ref int num)
+    {
+       num *= 2;
+    }
+
+    static void Desconto(int number)
+    {
+        number = number - (number * 10 / 100);   
+        Console.WriteLine(number);
+    }
+}
