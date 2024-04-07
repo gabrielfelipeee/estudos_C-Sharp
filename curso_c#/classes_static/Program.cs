4586882ecb851552a9aff5e07ddfe1d21eaecad8
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-04-07 10:25:08 -0300

    Classes estáticas

---
+++ curso_c#/classes_static/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+// Não pode ser instânciada, propriedades e métodos devem ser static
+
+/**
+As classes estáticas são usadas principalmente quando você
+deseja agrupar métodos relacionados ou fornecer
+funcionalidades utilitárias que não dependem do estado de instância.
+**/
+
+public static class MathUtils {
+    public static double FahrenheitToCelsius(double fahrenheit)
+    {
+        double celsius = (fahrenheit - 32) / 1.8;
+        return Math.Round(celsius, 2);
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Usando métodos de classes estáticas
+        Console.WriteLine(MathUtils.FahrenheitToCelsius(100));
+
+    }
+}
