cba66b317838bbb1f886eb1b33aeb3bbc6475f21
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-04-07 17:18:44 -0300

    protected

---
+++ curso_c#/modificadoresDeAcesso/protected/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Veiculo
+{
+    public int VelAtual;
+    private int VelMax;
+    protected bool Ligado;
+
+    public int GetVelMax()
+    {
+        return VelMax;
+    }
+    public string GetLigado()
+    {
+        return Ligado ? "Sim" : "Não";
+    }
+    public Veiculo(int velMax)
+    {
+        VelAtual = 0;
+        VelMax = velMax;
+        Ligado = false;
+    }
+}
+
+class Carro : Veiculo
+{
+    public string Nome;
+
+    public Carro(string nome, int velMax) : base(velMax)
+    {
+        Nome = nome;
+        Ligado=true;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Carro c1 = new Carro("Camaro", 260);
+
+        Console.WriteLine(c1.Nome);
+        Console.WriteLine(c1.GetLigado());
+        Console.WriteLine(c1.VelAtual);
+        Console.WriteLine(c1.GetVelMax());
+    }
+}
