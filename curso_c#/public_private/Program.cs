c73579e7bc2dfc20afb7c8d2c0690d638b28cbfc
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-04-07 11:04:40 -0300

    private e public

---
+++ curso_c#/public_private/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices.Marshalling;
+
+class Jogador
+{
+    private string Nome;
+    private int Energia;
+
+    public Jogador(string Nome)
+    {
+        this.Nome = Nome;
+        Energia = 100;
+    }
+
+
+    // Com as propriedades privadas não podemos acesssar fora de sua classe, mas podemos criar métodos para isso.
+    public int GetEnergia()
+    {
+        return Energia;
+    }
+    public int SetEnergia(int energia)
+    {
+        if (energia <= 0)
+        {
+            Energia = 0;
+        }
+        else if (energia > 0 && energia <= 100)
+        {
+            Energia = energia;
+        }
+        else
+        {
+            Energia = 100;
+
+        }
+        return Energia;
+    }
+
+    public string GetNome()
+    {
+        return Nome;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Jogador player1 = new Jogador("Manel");
+        Console.WriteLine("Nome: {0}", player1.GetNome());
+        Console.WriteLine("Energia: {0}", player1.SetEnergia(180));
+    }
+}
