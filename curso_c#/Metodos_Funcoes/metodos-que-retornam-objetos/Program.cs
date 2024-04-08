0c9796ff9220357adcd8f6780e6da4a53ba4f1af
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-04-08 16:32:55 -0300

    Métodos que retornam objetos | sobrecargas de métodos

---
+++ curso_c#/Metodos_Funcoes/metodos-que-retornam-objetos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+class Galinha
+{
+    private string nomeGalinha;
+    private int numOvo;
+    public Galinha(string nomeGalinha)
+    {
+        this.nomeGalinha = nomeGalinha;
+        numOvo = 0;
+    }
+
+    // Método que retorna um ovo
+    // acesso: public    tipo: Ovo  nome: botarOvo
+    public Ovo BotarOvo()
+    {
+        numOvo++;
+        return new Ovo(numOvo, nomeGalinha);
+    }
+}
+
+class Ovo
+{
+    private int numOvos;
+    private string galinha;
+    public Ovo(int numOvo, string galinha)
+    {
+        numOvos = numOvo;
+        this.galinha = galinha;
+        Console.WriteLine("A galinha {0} botou {1} ovo(s)", this.galinha, this.numOvos);
+    }
+}
+
+class Program
+{ 
+    static void Main()
+    {
+        Galinha g1 = new Galinha("Ginger");
+        Galinha g2 = new Galinha("Rocky");
+        Galinha g3 = new Galinha("Molly");
+        g1.BotarOvo();
+
+        g3.BotarOvo();
+    }
+}
---
+++ curso_c#/Metodos_Funcoes/sobrecarga_metodos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+/*
+A sobrecarga de métodos é um conceito que permite a definição de múltiplas versões de um método dentro de uma classe, 
+com o mesmo nome, mas com diferentes parâmetros. 
+*/
+class Calc
+{
+    public int Soma(params int[]nums)
+    {
+        int res = 0;
+        foreach(int num in nums)
+        {
+            res += + num;
+        }
+        return res;
+    }
+    public double Soma(params double[]nums)
+    {
+        double res = 0;
+        foreach (double num in nums)
+        {
+            res += +num;
+        }
+        return res;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Calc calc = new Calc();
+
+        int resInt = calc.Soma(10, 20);
+        double resDouble = calc.Soma(10.65, 20.58);
+
+        Console.WriteLine(resInt);
+        Console.WriteLine(resDouble);
+    }
+}
+
