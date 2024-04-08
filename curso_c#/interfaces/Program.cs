3fd70f05db8e0bc1245d6b010c60ebbd067a6300
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-04-08 13:49:21 -0300

    Interfaces

---
+++ curso_c#/interfaces/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+
+/**
+interface define um contrato contendo métodos, propriedades,
+eventos ou outros membros que devem ser implementados pelas classes que a usam/herdam.
+**/
+interface IVeiculo
+{
+    void Ligar();
+    void Desligar();
+    void Info();
+    bool Ligado { get; set; }
+}
+
+interface ICombate
+{
+    public void Disparar();
+}
+
+
+// Essa classe deve ter todos os métodos propriedades da interface IVeiculo
+class Carro : IVeiculo 
+{
+    private bool ligado;
+    public bool Ligado
+    { 
+        get
+        {
+            return ligado;
+        }
+        set
+        {
+            ligado = value;
+        }
+    }
+
+
+    public void Ligar()
+    {
+        ligado = true;
+    }
+    public void Desligar()
+    {
+        ligado = false;
+    }
+    public void Info() { 
+        if (ligado)
+        {
+            Console.WriteLine("O carro está Ligado");
+        } else
+        {
+            Console.WriteLine("O carro está Desligado");
+        }
+    }
+}
+
+
+// Classe usando 2 interfaces
+class CarroDeCombate : IVeiculo, ICombate
+{
+    public void Ligar() { }
+    public void Desligar() { }
+    public void Info() { }
+    public bool Ligado { get; set; }
+
+    public void Disparar() { }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Carro carro = new Carro();
+        carro.Ligado = true;
+        carro.Info();
+    }
+}
