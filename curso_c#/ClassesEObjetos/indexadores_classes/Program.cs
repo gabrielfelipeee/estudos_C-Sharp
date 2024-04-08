07c3f335b8178b9945503d78e4cb61d7ef741ea2
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-04-08 13:26:09 -0300

    get e set | Indexadores de classes

---
+++ curso_c#/ClassesEObjetos/indexadores_classes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/**
+ Indexadores permitem que uma classe seja indexada como um array
+**/
+
+
+class Carro
+{
+    private int[] VelMax = new int[5] { 120, 240, 260, 300, 360};
+
+
+    //Indexador
+    public int this[int i]
+    {
+        get
+        {
+            return VelMax[i];
+        }
+
+        set
+        {
+            if (value > 0 && value <= 360)
+            {
+                VelMax[i] = value;
+            }
+            else
+            {
+                VelMax[i] = 360;
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Carro c1 = new Carro();
+        
+        Console.WriteLine("Velocidade máxima: {0}", c1[4]);
+    }
+}
---
+++ curso_c#/PilaresDePOO/encapsulamento_get-set/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Carro
+{
+    private int VelMax;
+    public int VM 
+    { 
+        get {
+            return VelMax;
+        }
+
+        set
+        {
+            if (value > 0 && value <= 360)
+            {
+                VelMax = value;
+            }
+            else
+            {
+                VelMax = 360;
+            }
+        }
+    }
+    public Carro()
+    {
+        VelMax = 260;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Carro c1 = new Carro();
+        c1.VM = 220;
+        Console.WriteLine("Velocidade máxima: {0}", c1.VM);
+    }
+}
