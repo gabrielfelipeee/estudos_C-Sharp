9f10ccdadb78f0d6b022bebe5f272ba9cbb20f55
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-04-08 18:00:25 -0300

    Métodos e variáveis estáticas

---
+++ curso_c#/Metodos_Funcoes/metodos_static/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+/*
+Os métodos e variáveis estáticas são associados à classe em vez de a uma instância específica da classe. Isso significa que eles pertencem à classe em si, não a objetos individuais criados a partir dela. 
+*/
+
+
+class Math
+{
+    public static double pi = 3.14;
+
+    public static int Dobro(int num) {
+        return num * 2;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Usando variáveis e métodos static sem precisar declarar um objeto
+        double vpi = Math.pi;
+        int number = Math.Dobro(5);
+
+        Console.WriteLine(vpi);
+        Console.WriteLine(number);
+
+    }
+}
