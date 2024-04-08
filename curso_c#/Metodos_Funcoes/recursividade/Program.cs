9cf8662ecc0611c7a6cb76633bece3fbaed3a737
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-04-08 17:46:43 -0300

    Recursividade

---
+++ curso_c#/Metodos_Funcoes/recursividade/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/*
+Recursividade refere-se à capacidade de uma função chamar a si mesma diretamente ou indiretamente para resolver um problema.
+*/
+
+class Recursividade
+{
+    public int Fatorial(int num)
+    {
+        int res;
+        if (num <= 1)
+        {
+            res = 1;
+        }
+        else
+        {
+            res = num * Fatorial(num - 1); // Recursividade
+        }
+        return res;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Recursividade c1 = new Recursividade();
+        Console.WriteLine(c1.Fatorial(6));
+    }
+}
+
