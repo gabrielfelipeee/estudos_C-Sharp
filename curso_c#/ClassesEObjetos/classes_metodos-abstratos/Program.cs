62391b7c10122e05c2ef9f8723e3d3ea3ee07245
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-04-08 11:35:41 -0300

    classes abstratas

---
+++ curso_c#/ClassesEObjetos/classes_metodos-abstratos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+/**
+Uma classe abstrata é uma classe que não pode ser instanciada diretamente. 
+Ela é projetada para ser herdada por outras classes, que então podem fornecer 
+implementações concretas para os métodos abstratos definidos na classe abstrata.
+**/
+
+abstract class Veiculo // Classe Base Abstrata
+{
+    protected int VelMax;
+    protected int VelAtual;
+    protected bool Ligado;
+    public Veiculo()
+    {
+        VelAtual = 0;
+        Ligado = false;
+    }
+
+    public void SetLigado(bool ligado)
+    {
+        Ligado = ligado;
+    }
+
+
+    // Método abstrato, todas as classes derivadas de Veiculo devem implementá-lo.
+    abstract public void Aceleracao(int mult);
+}
+
+class Carro : Veiculo
+{
+    public Carro()
+    {
+        VelMax = 160;
+    }
+
+
+    // Implentação do método abstrato
+    public override void Aceleracao(int mult)
+    {
+        VelAtual += 10 * mult;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Carro carro = new Carro();
+        carro.Aceleracao(2);
+    }
+}
