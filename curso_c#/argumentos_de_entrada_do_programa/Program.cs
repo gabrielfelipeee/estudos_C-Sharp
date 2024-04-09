5291a635d0d33c62c92a0181d9b28c6e81e9f590
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-04-09 15:16:22 -0300

    Argumentos de entrada de um programa

---
+++ curso_c#/argumentos_de_entrada_do_programa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            Console.WriteLine("Qtde de argumentos: {0}", args.Length);
+            for (int i = 0; i <= args.Length; i++)
+            {
+                Console.WriteLine("Argumento {0}: {1}", i + 1, args[i]);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Não foi passado argumento");
+        }
+    }
+}
