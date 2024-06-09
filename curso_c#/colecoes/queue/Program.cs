741d13fc3bcb7d49e9884090f3b749b9c90cb02a
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-06-09 07:24:07 -0300

    Queue

---
+++ curso_c#/colecoes/queue/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/*
+FIFO (First In, First Out): Em uma fila, o primeiro elemento a ser adicionado é o primeiro a ser removido.
+Usada quando você precisa processar itens na ordem em que foram adicionados.
+*/
+
+
+class Queue
+{
+    static void Main()
+    {
+        Queue<string> veiculos = new Queue<string>();
+
+        // Adicionar
+        veiculos.Enqueue("Carro");
+        veiculos.Enqueue("Moto");
+        veiculos.Enqueue("Bike");
+
+        // Remover
+        veiculos.Dequeue(); // Carro foi removido (o primeiro elemento a ser adicionado é o primeiro a ser removido). Moto passou a ser o primeiro
+
+        // Retorna o primeiro elemento, mas não exclui
+        Console.WriteLine(veiculos.Peek());
+    }
+}
